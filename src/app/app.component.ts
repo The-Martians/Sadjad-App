0cd918623d0a040392109a6e75b853763bee3dfd
parham <heidari1377@gmail.com>
2016-12-17 11:22:32 +0330

    create a main page using temp data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
+import { Setting } from '../pages/setting/setting';
 
 
 @Component({
@@ -21,8 +22,9 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
+      { title: 'صفحه اول', component: Page1 },
+      { title: 'صفحه دوم', component: Page2 },
+      { title: 'تنظیمات', component: Setting }
     ];
 
   }
---
+++ src/pages/setting/setting.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'setting',
+  templateUrl: 'setting.html'
+})
+export class Setting {
+  icons: string[];
+  items: Array<{title: string, pass: string, user: string , login:boolean ,input:string}>;
+  ///items={title:"salam",input:"شماره دانشجویی",login:false,user:"-1",pass:"-1";};
+}
